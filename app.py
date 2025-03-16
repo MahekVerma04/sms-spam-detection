bb1d27ae3986f8b06a88f293e0f7fd3c77b227fb
Mahek Verma <147248342+MahekVerma04@users.noreply.github.com>
2025-03-17 00:04:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+import pickle
+import streamlit as st
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+ps=PorterStemmer()
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+    text = y[:]
+    y.clear()
+    stop_words = set(stopwords.words('english'))
+    for i in text:
+        if i not in stop_words and i not in string.punctuation:
+            y.append(i)
+    text = y[:]
+    y.clear()
+    for i in text:
+        y.append(ps.stem(i))
+    return " ".join(y)
+
+
+
+
+tfidf=pickle.load(open('vectorizer.pkl','rb'))
+model=pickle.load(open('model.pkl','rb'))
+
+st.title("E-mail/SMS Spam Classification")
+
+input_sms=st.text_input("Enter the message")
+
+if st.button('Predict'):
+
+    #preprocess
+    transformed_sms=transform_text(input_sms)
+    #vectorize
+    vector_input=tfidf.transform([transformed_sms])
+    #predict
+    result=model.predict(vector_input)[0]
+    #display
+    if result==1:
+        st.header("Spam")
+    else:
+        st.header("Not spam")
